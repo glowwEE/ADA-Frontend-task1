184a0b3f361638fe6a2aa28e5c8b91dedd0d5e8e
glowwEE <gloryokafor5@gmail.com>
2023-01-05 18:08:14 +0100

    initial commit

---
+++ JS_first_task/index.js
@@ -0,0 +1,7 @@
+let username;
+var age;
+
+username= prompt("Enter your username", "Glory Oakfor");
+age= prompt("Enter your age");
+
+alert("Welcome, " +username+ " ,You are "+age+ " years old.");
---
+++ JS_first_task/readme.md
@@ -0,0 +1,3 @@
+## ADA FRONTEND WEEK 11 Task
+
+### a Javascript program that prompts user to input ther username and age 
